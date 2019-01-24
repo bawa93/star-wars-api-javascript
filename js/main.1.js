575b4fd2ae3aa4a54d1fc124d6646f21db49e72c
Lakhveer Bawa <bawa93lakhveer@gmail.com>
2019-01-23 19:01:43 -0500

    search results and design completed

--- js/main.1.js
+++ js/main.1.js
@@ -1,51 +1,53 @@
-
-(function() {
+(function () {
 
     function StarWarsAPI() {
-        this.charactor_images_available = [
-        'Luke Skywalker', 
-        'C-3PO', 
-        'R2-D2', 
-        'Darth Vader',
-        'Leia Organa',
-        'Owen Lars',
-        'Beru Whitesun lars',
-        'R5-D4',
-        'Biggs Darklighter',
-        'Obi-Wan Kenobi'
-        ],
-            this.getCharacters = function (search_term ='',container) {
-                console.log('hi');
-                const url = 'https://swapi.co/api/people/?search=' + search_term;
+
+        this.getRandomInt = function (max) {
+                return Math.floor(Math.random() * Math.floor(max));
+            },
+            this.getCharacters = function (search_term = '', container) {
+                const url = 'https://swapi.co/api/people/';
 
                 var currentObj = this;
                 fetch(url)
                     .then((resp) => resp.json())
                     .then(function (data) {
                         var charactors = data.results;
+
+                        var filtered_chars = charactors.filter(function (element) { // searching the characters if they match a searched term
+                            if (search_term != '') {
+                                return element.name.toLowerCase().indexOf(search_term) == -1 ? false : true;
+                            } else {
+                                return true;
+                            }
+
+                        });
+
                         document.getElementById(container).innerHTML = '';
 
-
                         var charactors_html = '';
-                         charactors.forEach(element => {
-                        
-                             if ((currentObj.charactor_images_available.indexOf(element.name) != -1)) { //we have image for this result
+                        filtered_chars.forEach(element => {
 
+                            var total_movies_count = element.films.length;
+                            var random_movie_key = currentObj.getRandomInt(total_movies_count - 1);
 
-                                charactors_html += `
-                                <div class="box">
+                            var random_movie = element.films[random_movie_key];
+
+                            console.log("Random array key" + random_movie_key);
+                            charactors_html += `
+                                <div class="box2">
                 <article class="media">
                     <div class="media-left">
-                        <figure class="image is-32x32" style="overflow: hidden;">
+                        <figure class="image is-64x64" style="overflow: hidden;">
                             <img src="assets/charactors/${element.name.replace(' ', '_').toLowerCase()}.png" alt="Image" class="image">
                         </figure>
                     </div>
                     <div class="media-content">
                         <div class="content">
                             <p>
-                            <a href = "#"
+                            <a href="#search_section"
                                 data-charactor = "${element.name}"
-                                data-movie_link = "${element.films[0]}"
+                                data-movie_link = "${random_movie}"
                                 class = "charactor-movies">    
                             <strong>${element.name}</strong>
                             </a>
@@ -56,13 +58,11 @@
                     </div>
                 </article>
             </div>`;
-                            }else { // we don't have image in our database for this charactor
-                                console.log('No Results Found');
-                            }
-                            
+
+
                         });
                         document.getElementById(container).innerHTML = charactors_html
-                        
+
                         var allCharactorLinks = document.querySelectorAll('.charactor-movies');
 
                         allCharactorLinks.forEach((value, key) => {
@@ -75,40 +75,44 @@
                         console.log(error);
                     });
             },
-            
-            this.getMovie = function(e) {
-                e.preventDefault();
-                
+
+            this.getMovie = function (e) {
+                // e.preventDefault();
+
                 var url = this.getAttribute("data-movie_link");
                 var charactor_name = this.getAttribute("data-charactor");
+                var url_parts_array = url.split('/');
 
+                var movie_id = url_parts_array[5];
 
+                var currentObj = this;
                 fetch(url)
                     .then((resp) => resp.json())
                     .then(function (data) {
                         document.getElementById("search__results").classList.remove("is-half");
-                        
+                        if (document.contains(document.getElementById("first_col"))) {
+                            document.getElementById("first_col").remove();
+                        }
+
+
                         document.getElementById("movie-container").classList.add("is-three-quarters");
                         // document.getElementById("movie-container").classList.remove("is-half");
 
 
-                        document.getElementById("movie-container").innerHTML = `
-                        <div style="background-color: red">
 
-                        <img src = "assets/charactors/${charactor_name.replace(' ', '_').toLowerCase()}.png">
-                       ${data.title}
-                       
-                        </div>
-                            `;
 
-                            var movie_container = `
+                        var movie_container = `
+                            <div class="card"  style="opacity:.9">
+                            
+                            <div class="card-content">
+                            
                             <div class="columns">
                 <div class="column movie">
-                    <div class="columns has-text-white">
+                    <div class="columns">
                         <div class="column">
-                            <img src="assets/charactors/${charactor_name.replace(' ', '_').toLowerCase()}.png" class="image">
+                            <img src="assets/movies/${ movie_id }.jpg" class="image">
                         </div>
-                        <div class="column">
+                        <div class="column is-two-thirds">
                             <h1 class="is-size-3 movie__title">${data.title}</h1>
 
                             <div>
@@ -118,20 +122,20 @@
                                 </p>
                                 <div class="is-size-5 movie__details">Details:</div>
                                 <div class="movie__detail-item">
-                                    <strong class="has-text-white">Producer</strong>:
+                                    <strong class="">Producer</strong>:
                                     <span class="is-size-6">
                                         ${data.producer}
                                     </span>
                                 </div>
 
                                 <div class="detail-item">
-                                    <strong class="has-text-white">Director</strong>:
+                                    <strong class="">Director</strong>:
                                     <span class="is-size-6">
                                         ${data.director}
                                     </span>
                                 </div>
                                 <div class="detail-item">
-                                    <strong class="has-text-white">Release Date</strong>:
+                                    <strong class="">Release Date</strong>:
                                     <span class="is-size-6">
                                         ${data.release_date}
                                     </span>
@@ -141,6 +145,9 @@
                     </div>
                 </div>
             </div>
+
+            </div>
+                            </div>
                             `;
 
                         document.getElementById("movie-container").innerHTML = movie_container;
@@ -158,10 +165,38 @@
     StarWars.getCharacters('', "search__results")
 
     let inputField = document.getElementById("searchChar");
-    inputField.addEventListener("keyup", function() {
+    inputField.addEventListener("keyup", function () {
         StarWars.getCharacters(this.value, "search__results")
-    },false);
+    }, false);
 
 
 
+
+    // navbar
+
+    document.addEventListener('DOMContentLoaded', () => {
+
+        // Get all "navbar-burger" elements
+        const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+        // Check if there are any navbar burgers
+        if ($navbarBurgers.length > 0) {
+
+            // Add a click event on each of them
+            $navbarBurgers.forEach(el => {
+                el.addEventListener('click', () => {
+
+                    // Get the target from the "data-target" attribute
+                    const target = el.dataset.target;
+                    const $target = document.getElementById(target);
+
+                    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                    el.classList.toggle('is-active');
+                    $target.classList.toggle('is-active');
+
+                });
+            });
+        }
+
+    });
 })();
